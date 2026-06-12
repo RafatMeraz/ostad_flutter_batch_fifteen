fbb72ce20cadce89f49375a4491e74d359aac056
RafatMeraz <razhossain201@gmail.com>
2026-06-12 19:10:14 +0200

    Add firebase firestore database

---
+++ lib/models/football_match.dart
@@ -0,0 +1,31 @@
+class FootballMatch {
+  final String id;
+  final String team1Name;
+  final String team2Name;
+  final int team1Score;
+  final int team2Score;
+  final String winnerTeam;
+  final bool isRunning;
+
+  FootballMatch({
+    required this.id,
+    required this.team1Name,
+    required this.team2Name,
+    required this.team1Score,
+    required this.team2Score,
+    required this.winnerTeam,
+    required this.isRunning,
+  });
+
+  factory FootballMatch.fromJson(String docId, Map<String, dynamic> jsonData) {
+    return FootballMatch(
+      id: docId,
+      team1Name: jsonData['team1_name'],
+      team2Name: jsonData['team2_name'],
+      team1Score: jsonData['team1_score'],
+      team2Score: jsonData['team2_score'],
+      winnerTeam: jsonData['winner_team'],
+      isRunning: jsonData['is_running'],
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ostad_flutter_batch_fifteen/models/football_match.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +11,92 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // List<FootballMatch> footballMatchList = [];
+  //
+  // bool _getFootballMatchesInProgress = false;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _getFootballMatches();
+  // }
+  //
+  // Future<void> _getFootballMatches() async {
+  //   _getFootballMatchesInProgress = true;
+  //   setState(() {});
+  //
+  //   QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore
+  //       .instance
+  //       .collection('football')
+  //       .get();
+  //
+  //   for (DocumentSnapshot doc in querySnapshot.docs) {
+  //     footballMatchList.add(
+  //       FootballMatch.fromJson(doc.id, doc.data() as Map<String, dynamic>),
+  //     );
+  //   }
+  //
+  //   _getFootballMatchesInProgress = false;
+  //   setState(() {});
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: [
+          IconButton(onPressed: _onTapLogoutButton, icon: Icon(Icons.logout)),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('football').snapshots(),
+        builder: (context, snapshots) {
+          if (snapshots.connectionState == .waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshots.hasError) {
+            return Center(child: Text(snapshots.error.toString()));
+          }
+
+          List<FootballMatch> footballMatchList = [];
+
+          for (DocumentSnapshot doc in snapshots.data!.docs) {
+            footballMatchList.add(
+              FootballMatch.fromJson(
+                doc.id,
+                doc.data() as Map<String, dynamic>,
+              ),
+            );
+          }
+
+          return ListView.separated(
+            itemCount: footballMatchList.length,
+            itemBuilder: (context, index) {
+              final FootballMatch match = footballMatchList[index];
+
+              return ListTile(
+                leading: CircleAvatar(
+                  radius: 8,
+                  backgroundColor: match.isRunning ? Colors.green : Colors.grey,
+                ),
+                title: Text('${match.team1Name} vs ${match.team2Name}'),
+                subtitle: Text('Winner Team: ${match.winnerTeam}'),
+                trailing: Text(
+                  '${match.team1Score}-${match.team2Score}',
+                  style: TextStyle(fontSize: 18),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => SizedBox(height: 8),
+          );
+        },
+      ),
+    );
+  }
+
+  void _onTapLogoutButton() {
+    FirebaseAuth.instance.signOut();
   }
 }
