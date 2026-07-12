12b834e2b273ed1ae9d6cd618a9a6ac4418c676d
RafatMeraz <razhossain201@gmail.com>
2026-07-12 19:04:31 +0200

    Add theming and route set up

---
+++ crafty_bay/lib/features/auth/presentation/widgets/app_logo.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../app/asset_paths.dart';
+
+class AppLogo extends StatelessWidget {
+  const AppLogo({super.key, this.width = 120, this.height = 120});
+
+  final double width;
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      AssetPaths.logoSvg,
+      width: width,
+      height: height,
+      fit: .scaleDown,
+    );
+  }
+}
